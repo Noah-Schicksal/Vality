ac17ba00cc1120a41299d045cba673e5c154800a
Noah-Schicksal <NoahSCH10833@gmail.com>
2025-04-09 01:08:04 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:vality/login.dart';
-import 'package:vality/Home.dart'; // Sua página inicial
+import 'package:vality/Home.dart'; 
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'model/notify/notifications_service.dart';
 
 void main() async {
-  // Garantir que a inicialização do Firebase seja feita corretamente
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -22,11 +21,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vality App',  // Nome do seu app
+      title: 'Vality', 
       theme: ThemeData(
         colorScheme: ColorScheme.light(
-          primary: Colors.black38,  // Cor primária
-          secondary: Colors.green,  // Cor secundária
+          primary: Colors.black38,  
+          secondary: Colors.green, 
         ),
       ),
       home: AuthenticationWrapper(),
@@ -37,18 +36,14 @@ class MyApp extends StatelessWidget {
 class AuthenticationWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Aqui verificamos se o usuário está autenticado
     return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),  // Verifica o estado de autenticação
+      stream: FirebaseAuth.instance.authStateChanges(),  
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          // Carregando enquanto verifica a autenticação
           return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasData) {
-          // Se o usuário estiver autenticado, redireciona para a Home
           return Home();
         } else {
-          // Se não, redireciona para a tela de login
           return Login();
         }
       },
